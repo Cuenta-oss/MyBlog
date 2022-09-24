d601331d4ede136d63a672190af332c8cade8596
[Cuenta-oss] <[ejmenrique97@gmail.com]>
2022-09-24 17:50:04 -0500

    Password modified

--- blog/settings.py
+++ blog/settings.py
@@ -141,6 +141,6 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'scriptexe462@gmail.com'
-EMAIL_HOST_PASSWORD = 'sfvtpuwiltsasmrc'
+EMAIL_HOST_PASSWORD = 'password'
 
 CRYSPY_TEMPLATE_PACK = 'bootstrap5'
